e07066d1fd12e8e7ba95a25af86ea7fa41c34d75
danielOuattara <daniel.ouattara@gmx.com>
2023-08-22 22:40:43 +0200

    02_02 start graphql server

---
+++ 02_express-graphql_server_setup/02_start_graphql/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+//--------------------------------------------------------------
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  }),
+);
+app.listen(port, console.log(`Sever running on port ${port}`));
