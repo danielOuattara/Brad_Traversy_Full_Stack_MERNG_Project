f5c9512d9c64e1a73ba8a3fd492c2dac1bac3cb5
danielOuattara <daniel.ouattara@gmx.com>
2023-08-23 16:39:38 +0200

    03_14 start Apollo React client setup

---
+++ 03_appolo_client/14_start_on_client/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/assets/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
