e4ff393102b950dda222fce865e28ac16bb1b2a4
danielOuattara <daniel.ouattara@gmx.com>
2023-08-23 16:40:59 +0200

    03_17  mutation, delete Client

---
+++ 03_appolo_client/17_mutation_delete_client/src/App.jsx
@@ -0,0 +1,21 @@
+import Clients from "./components/Clients";
+import Header from "./components/assets/Header";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: " http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Header />
+      <div className="container">
+        <Clients />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
