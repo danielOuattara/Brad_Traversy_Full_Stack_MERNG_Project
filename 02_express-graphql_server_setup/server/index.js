0c4dec52f2b260d07d1d6cb4f961abc672859faf
danielOuattara <daniel.ouattara@gmx.com>
2023-08-23 16:38:01 +0200

    02 express graphql server OK

---
+++ 02_express-graphql_server_setup/server/index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const colors = require("colors");
+const { connectToDB } = require("./config/db");
+const cors = require("cors");
+//--------------------------------------------------------------
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  }),
+);
+
+//---
+
+// const start = () => {
+//   try {
+//     connectToDB(process.env.MONGO_URI, process.env.DATABASE);
+//     app.listen(
+//       port,
+//       console.log(`Sever running on -->  http://localhost:${port}/graphql`),
+//     );
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// start();
+
+//---
+
+connectToDB(process.env.MONGO_URI, process.env.DATABASE)
+  .then(() => {
+    app.listen(
+      port,
+      console.log(`Sever running on -->  http://localhost:${port}/graphql`),
+    );
+  })
+  .catch((err) => console.log(err.message));
